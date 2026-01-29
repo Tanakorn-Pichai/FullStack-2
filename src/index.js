6cd86cc30cecd0afe8713c077cfcb0e3ade0a039
Tanakorn Pichai <s6706022511022@email.kmutnb.ac.th>
2026-01-29 15:59:05 +0700

    feat: add CRUD operations for books in a new Express application
    - Implemented GET, POST, PUT, and DELETE endpoints for managing books.
    - Created a simple in-memory data structure to store book information.
    - Added dotenv configuration for environment variables.
    - Included a basic index route for testing server functionality.
    - Introduced a new file structure for better organization of the application.
    - Added necessary package.json files for new dependencies and scripts.
    - Included README and LICENSE files for new modules.

--- src/index.js
+++ src/index.js
@@ -5,10 +5,9 @@ const app = express();
 const port = process.env.PORT || 7000;
 
 app.get("/", (req, res) => {
-  res.send("อ่ะฮิ");
+  res.send("ไ'''ง");
 });
 
-app.listen(port,() => {
-    console.log(`app listening at http://localhost:${port}`);
-  },
-);
+app.listen(port, () => {
+  console.log(`app listening at http://localhost:${port}`);
+});
