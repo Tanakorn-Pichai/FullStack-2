2ece9ff14d0fe4a49f79d55eb31cc2317b60a200
Tanakorn Pichai <s6706022511022@email.kmutnb.ac.th>
2026-02-12 14:10:49 +0700

    update index.js add sql.js

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ const port = process.env.PORT || 7000;
 
 app.get("/", (req, res) => {
     res.send(`
-    <h1>Server Running</h1>
-    <a href="/books">Go to Books</a>
+    Ha lo 
   `);});
 
 app.listen(port, () => {
